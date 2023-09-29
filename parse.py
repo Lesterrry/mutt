6001160403447a3575fbccd1b27fe8b52e5ce344
Aydar N. (Sublime Merge MBP) <me@aydar.media>
2023-09-29 22:29:59 +0300

    Fancy new course support

--- parse.py
+++ parse.py
@@ -14,12 +14,15 @@ import sys
 EMAIL = "aetimofeev@edu.hse.ru"
 PWD = "2022"
 GLOBAL_CALENDARS_PATH = "/var/www/html/mutt/"
-VERSION = "0.2.2"
-FINAL_WORD = "🥀 снова в школу"
+VERSION = "0.3.0"
+FINAL_WORD = "❇️ aydarmedia.t.me"
 REPO_URL = "https://timetracker.hse.ru"
-FINAL_URL = "https://timetracker.hse.ru/schedule.aspx?organizationId=1&facultyids=1&course=2"
+FINAL_URL = "https://timetracker.hse.ru/schedule.aspx?organizationId=1&facultyids=1&course="
 BANLIST = ["История и теория", "Английский язык", "Военная подготовка", "Майнор"]
 BASE_DAY = 4
+COURSES = [1, 2]
+
+VERBOSE = "--verbose" in sys.argv
 
 if "-v" in sys.argv or "--version" in sys.argv:
 	print(f"Mutt v{VERSION}")
@@ -45,8 +48,11 @@ class Lecture:
 		else:
 			self.desc = desc
 
-def die(*args, **kwargs):
-	print(*args, file=sys.stderr, **kwargs)
+def log(message):
+	if VERBOSE:
+		print(message)
+def die(message):
+	print(f'FATAL: {message}')
 	exit(1)
 def get_date(dow):
 	days = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"]
@@ -56,6 +62,7 @@ def get_date(dow):
 	else:
 		return day
 
+log('Fetching login form...')
 response = requests.get("https://timetracker.hse.ru/login.aspx")
 if response.status_code != 200:
 	die(f"Round 1: Got {response.status_code} code (expected 200), dying...")
@@ -83,71 +90,85 @@ data = {
 	"ctl00$cplhMainContent$ddlOrganizations": "1",
 }
 
+log('Attempting login...')
 response = requests.post("https://timetracker.hse.ru/login.aspx", headers=headers, data=data, allow_redirects=False)
 if response.status_code != 302:
 	# with open("./txt.html", "w") as file:
 	# 	file.write(response.text)
 	die(f"Round 2: Got {response.status_code} code (expected 302), dying...")
 jar = response.cookies
-redirect = response.headers["Location"]
-response = requests.get(FINAL_URL, cookies=jar)
-if response.status_code != 200:
-	die(f"Round 3: Got {response.status_code} code (expected 200), dying...")
 
-response = BeautifulSoup(response.text, features="lxml")
+groups = {}
 
-groups = []
+for course in COURSES:
+	log(f'\nParsing course {course}...')
+	course_url = FINAL_URL + str(course)
 
-for i in response.find("table", attrs={"class":"schedule-table"}).thead.tr:
-	if i.string is not None:
-		continue
-	groups.append(Group(translit(i.b.string.split(" ")[0], "ru", reversed=True)))
+	response = requests.get(course_url, cookies=jar)
+	if response.status_code != 200:
+		die(f"Round 3: Got {response.status_code} code (expected 200), dying...")
 
-chief_added = False
-for row in response.find("table", attrs={"class":"schedule-table"}).tbody:
-	index = 0
-	for cell in row:
-		if type(cell) is not str and cell.string is not None and cell.string != "\n":
-			txt = cell.string.strip().replace('\n', '')
-			if len(txt) == 13:
-				time = txt.split("-")
-			elif len(txt) == 2:
-				dow = txt
-			else:
-				die(f"Could not parse text instance '{txt}'")
-		elif type(cell) is not bs4.element.NavigableString:
-			nc = BeautifulSoup(str(cell), features="lxml")
-			a = nc.find_all("div")
-			sp = nc.find_all("span")
-			desc = None
-			for k in sp:
-				desc = k.get_text().replace("\n", "").strip()
-				k.replaceWith("")
-			need_append = False
-			for k in a:
-				if k.get("class") == ["discipline"]:
-					discipline = k.get_text().strip()
-					need_append = True
-					aud = None
-					lec = None
-				elif k.get("class") == ["auditory"]:
-					aud = k.get_text()
-				elif k.get("class") == ["user"]:
-					lec = k.get_text()
-				elif k.get("class") == ["comment"]:
-					if desc is None:
-						desc = k.get_text()
+	response = BeautifulSoup(response.text, features="lxml")
+
+	log(f'  Parsing groups...')
+	groups[course] = []
+	for i in response.find("table", attrs={"class":"schedule-table"}).thead.tr:
+		if i.string is not None:
+			continue
+		group_name = translit(i.b.string.split(" ")[0], "ru", reversed=True)
+		log(f'    Found group {group_name}')
+		groups[course].append(Group(group_name))
+
+	log(f'  Parsing timetable...')
+	for row in response.find("table", attrs={"class":"schedule-table"}).tbody:
+		index = 0
+		for cell in row:
+			if type(cell) is not str and cell.string is not None and cell.string != "\n":
+				txt = cell.string.strip().replace('\n', '')
+				if len(txt) == 13:
+					time = txt.split("-")
+				elif len(txt) == 2:
+					dow = txt
+				else:
+					die(f"Could not parse text instance '{txt}'")
+			elif type(cell) is not bs4.element.NavigableString:
+				nc = BeautifulSoup(str(cell), features="lxml")
+				a = nc.find_all("div")
+				sp = nc.find_all("span")
+				desc = None
+				for k in sp:
+					desc = k.get_text().replace("\n", "").strip()
+					k.replaceWith("")
+				need_append = False
+				for k in a:
+					if k.get("class") == ["discipline"]:
+						discipline = k.get_text().strip()
+						need_append = True
+						aud = None
+						lec = None
+					elif k.get("class") == ["auditory"]:
+						aud = k.get_text()
+					elif k.get("class") == ["user"]:
+						lec = k.get_text()
+					elif k.get("class") == ["comment"]:
+						if desc is None:
+							desc = k.get_text()
+						else:
+							desc += ("\n" + k.get_text())
+				if need_append:
+					if not any(banned in discipline for banned in BANLIST):
+						lecture = Lecture(discipline, dow, time[0].strip(), time[1].strip(), aud, lec, desc)
+						groups[course][index - 1].lectures.append(lecture)
+						log(f'      Appended "{discipline} to group {groups[course][index - 1].title}"')
 					else:
-						desc += ("\n" + k.get_text())
-			if need_append:
-				if not any(banned in discipline for banned in BANLIST):
-					lecture = Lecture(discipline, dow, time[0].strip(), time[1].strip(), aud, lec, desc)
-					groups[index - 1].lectures.append(lecture)
-			index += 1
+						log(f'      Skipped "{discipline}"')
+				index += 1
 
-for i in groups:
+for i in [item for sublist in groups.values() for item in sublist]:
+	log(f'\nCreating events for group "{i.title}"...')
 	c = Calendar()
 	for j in i.lectures:
+		log(f'  Creating lecture "{j.title}"...')
 		e = Event()
 		e.name = j.title
 		e.begin = f"2023-09-{get_date(j.dow)}T{j.start_time}:00.000000+03:00"
@@ -160,3 +181,4 @@ for i in groups:
 	trans = translit(i.title.split(" ")[0], "ru", reversed=True)
 	with open(f"{GLOBAL_CALENDARS_PATH}{i.title}.ics", "w") as file:
 		file.writelines(c.serialize_iter())
+	log(f'Wrote group "{i.title}"')
